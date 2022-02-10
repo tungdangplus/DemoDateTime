a9dc2a7f40b8eb1c8c75570b15dcca75fe5cb5e0
TungDangPlus <tungdangplus@gmail.com>
2022-02-10 11:53:43 -0800

    Update20220210

--- DemoDateTime/ViewController.swift
+++ DemoDateTime/ViewController.swift
@@ -9,12 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var defaultDate: UILabel!
+    @IBOutlet weak var dateCalendar: UILabel!
+    @IBOutlet weak var timeCalendar: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.defaultDate.text = Date().description(with: .none) //.autoupdatingCurrent ->tự động lấy ngày theo locale
+        self.dateCalendar.text = self.dateTextFormatter(format: "dd/MM/YYYY HH:mm:ss")
+        self.timeCalendar.text = self.timeTextFormatter(format: "HH:mm:ss")
+        let _: Timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.defaultDate.text = Date().description(with: .none) //.autoupdatingCurrent ->tự động lấy ngày theo locale
+            self.dateCalendar.text = self.dateTextFormatter(format: "dd/MM/YYYY HH:mm:ss")
+            self.timeCalendar.text = self.timeTextFormatter(format: "HH:mm:ss")
+        }
+        
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//            self.defaultDate.text = Date().description(with: .none) //.autoupdatingCurrent ->tự động lấy ngày theo locale
+//            self.dateCalendar.text = self.dateTextFormatter(format: "dd/MM/YYYY HH:mm:ss")
+//            self.timeCalendar.text = self.timeTextFormatter(format: "HH:mm:ss")
+//        }
+        
     }
-
-
+    
+    func dateTextFormatter(format: String) -> String {
+//        let formatter = DateComponentsFormatter() //Chuyển đổi lượng thời gian
+        let dateFormatter = DateFormatter() //Chuyển đổi ngày
+        dateFormatter.locale = .autoupdatingCurrent
+        dateFormatter.dateStyle = .full
+        dateFormatter.dateFormat = format
+        
+        let today = Date() //Default date theo timezone gốc +000
+        return dateFormatter.string(from: today) //
+    }
+    
+    func timeTextFormatter(format: String) -> String {
+        let timeFormatter = DateFormatter()
+        timeFormatter.locale = .autoupdatingCurrent
+        timeFormatter.timeStyle = .full
+        timeFormatter.dateFormat = format
+        
+        let today = Date() //Default date theo timezone gốc +000
+        return timeFormatter.string(from: today)
+    }
+    
 }
 
